3121796f4d58157af5f7332ebca6f366299b0875
0777rodrigo <128700789+0777rodrigo@users.noreply.github.com>
2023-08-31 11:28:37 -0300

    Add files via upload

---
+++ jogador valioso/js/jogadores.js
@@ -0,0 +1,56 @@
+
+
+const jogador = document . querySelector('#jogadores')
+const messi = document . querySelector('#LionelMessi')
+const cristiano = document . querySelector('#CristianoRonaldo')
+const neymar = document . querySelector('#Neymar')
+const mbappe = document . querySelector('#KylianMbappé')
+const dybala = document . querySelector('#PauloDybala')
+const haaland = document . querySelector('#ErlingHaaland')
+
+
+LionelMessi . addEventListener('click', LionelMessi)
+CristianoRonaldo . addEventListener('click', CristianoRonaldo)
+Neymar . addEventListener('click', Neymar)
+KylianMbappé . addEventListener('click', KylianMbapppé)
+PauloDybala . addEventListener('click',PauloDybala)
+ErlingHaaland . addEventListener('click', ErlingHaaland)
+
+
+function LionelMessi(){
+
+jogador . src = "images/messi.jpg"
+
+
+}
+
+function CristianoRonaldo(){
+
+jogador . src = "images/cr7.jpg"
+
+}
+
+function Neymar(){
+
+jogador . src = "images/neymar.jpg"
+
+}
+
+function KylianMbapppé(){
+
+jogador . src = "images/mbappe.jpg"
+
+}
+
+function PauloDybala(){
+
+jogador . src = "images/dybala.jpg"
+
+}
+
+function ErlingHaaland(){
+
+jogador . src = "images/haaland.png"
+
+}
+
